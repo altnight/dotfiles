34ee787f2ca312b7b62ea88afc5a8f2aa73ed946
altnight <altnightsight@gmail.com>
2014-08-16 15:53:27 +0900

    fix path

--- .zsh/my/snippets.sh
+++ .zsh/my/snippets.sh
@@ -1,2 +1,2 @@
-reg(){echo $1 >> ~/.zsh/functions/snippets.sh}  # $1, single quote
+reg(){echo $1 >> ~/.zsh/my/snippets.sh}  # $1, single quote
 killtmux(){tmux list-sessions | percol | cut -d ":" -f 1 | xargs tmux kill-session -t}
