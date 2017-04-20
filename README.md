bcdcb393d8f7bc218b5c1a0adc4355a5d204a0d3
altnight <altnightsight@gmail.com>
2017-04-20 22:59:19 +0900

    Update Readme

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 dotfiles
 ========
 
+* [install dein.vim](https://github.com/Shougo/dein.vim)
 * sh ./make_symbolic_link.sh
--- make_symbolic_link.sh
+++ make_symbolic_link.sh
@@ -2,8 +2,6 @@
 
 DOT_FILES=( .hgrc .vimrc)
 
-ln -s $HOME/dotfiles/vimfiles/neobundle.vim $HOME/.vim/neobundle.vim
-
 for file in ${DOT_FILES[@]}
   do
     ln -s $HOME/dotfiles/$file $HOME/$file
